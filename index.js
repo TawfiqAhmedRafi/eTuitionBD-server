30ccee9dc321d97367168efbd285f3c8d3169849
TawfiqAhmedRafi <rafi70722@gmail.com>
2025-12-11 17:08:34 +0600

    initialized

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+  res.send('ETuitionBD Backend Service is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
